53b2c0356f2bd3d13fcb3963b222753c513adda6
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2011-12-29 15:03:29 +0000

    libmbin: Add new functions.

--- math_bin.h
+++ math_bin.h
@@ -491,4 +491,12 @@ int32_t	mbin_noise_24(void);
 
 uint32_t mbin_sumdigit_32(uint32_t, uint32_t, uint32_t);
 
+/* Complex functions */
+
+struct mbin_complex_double
+mbin_mul_complex_double(struct mbin_complex_double,
+    struct mbin_complex_double);
+double
+	mbin_square_len_complex_double(struct mbin_complex_double);
+
 #endif					/* _MATH_BIN_H_ */
