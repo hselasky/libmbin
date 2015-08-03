f1498a59e037be31ed9b3a0005259b1c37e5e017
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2015-08-03 09:23:27 +0000

    libmbin: Optimise sumbits() function.

--- mbin_sumbits.c
+++ mbin_sumbits.c
@@ -33,10 +33,10 @@ mbin_sumbits64(uint64_t val)
 	val = ((val & (0x1ULL * 0xAAAAAAAAAAAAAAAAULL)) >> 1) + (val & (0x1ULL * 0x5555555555555555ULL));
 	val = ((val & (0x3ULL * 0x4444444444444444ULL)) >> 2) + (val & (0x3ULL * 0x1111111111111111ULL));
 	val = ((val & (0xFULL * 0x1010101010101010ULL)) >> 4) + (val & (0xFULL * 0x0101010101010101ULL));
-	val = ((val & (0xFFULL * 0x0100010001000100ULL)) >> 8) + (val & (0xFFULL * 0x0001000100010001ULL));
-	val = ((val & (0xFFFFULL * 0x0001000000010000ULL)) >> 16) + (val & (0xFFFFULL * 0x0000000100000001ULL));
-	val = ((val & (0xFFFFFFFFULL * 0x0000000100000000ULL)) >> 32) + (val & (0xFFFFFFFFULL * 0x0000000000000001ULL));
-	return (val);
+	val += val >> 8;
+	val += val >> 16;
+	val += val >> 32;
+	return (val & 63);
 }
 
 uint8_t
@@ -55,9 +55,9 @@ mbin_sumbits32(uint32_t val)
 	val = ((val & (1U * 0xAAAAAAAAU)) / 2U) + (val & (1U * 0x55555555U));
 	val = ((val & (3U * 0x44444444U)) / 4U) + (val & (3U * 0x11111111U));
 	val = ((val & (15U * 0x10101010U)) / 16U) + (val & (15U * 0x01010101U));
-	val = ((val & (255U * 0x01000100U)) / 256U) + (val & (255U * 0x00010001U));
-	val = ((val & (65535U * 0x00010000U)) / 65536U) + (val & (65535U * 0x00000001U));
-	return (val);
+	val += val >> 8;
+	val += val >> 16;
+	return (val & 31);
 #endif
 }
 
